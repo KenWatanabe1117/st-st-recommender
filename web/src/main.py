db4ca471080e6c81f05b07c15ac391973b7c439b
KenWatanabe1117 <wordsofthemindbrandnewjourney@gmail.com>
2025-05-17 05:57:35 +0900

    made home view

--- web/src/main.py
+++ web/src/main.py
@@ -1,22 +1,54 @@
 import flet as ft
 
+FORM_URL = ""
 
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
-
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
     page.add(
         ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
+            ft.Column([
+                ft.Column([
+                    ft.Container(
+                        content = ft.Text("ストスト🎧レコメンダー", size=50),
+                        alignment = ft.alignment.center,
+                    ),
+                    ft.Container(
+                        content = ft.Text("サブスクで聴けるSixTONES楽曲66曲からあなたへおすすめの曲をレコメンド！"),
+                        alignment = ft.alignment.center,
+                    ),
+                    ft.Container(
+                        content = ft.Text("注意事項", size=30, color=ft.Colors.WHITE),
+                        alignment = ft.alignment.center,
+                        bgcolor = ft.Colors.RED_400
+                    ),
+                    ft.Container(
+                        content = ft.Text("※ 非公式のファンサイトです。\n※ 当サイトが提案する楽曲を必ず気に入ることを保証するものではありません。\n※ アップデートは順次行いますが個人運営のため対応には限度があります。", color=ft.Colors.WHITE),
+                        alignment = ft.alignment.center,
+                        bgcolor = ft.Colors.GREY
+                    ),
+                    ft.Container(
+                        content = ft.FilledButton(
+                            content = ft.Text("Start!", size=50),
+                            on_click = page.go('question/1'),
+                            style = ft.ButtonStyle(
+                                shape = ft.CircleBorder(),
+                                padding = 50
+                            )
+                        ),
+                        alignment = ft.alignment.center
+                    )
+                ]),
+                ft.Container(
+                    content = ft.TextButton(
+                        content = ft.Text("ご意見・ご要望はこちらのフォームから", color=ft.Colors.BLUE_400),
+                        url = FORM_URL,
+                        style = ft.ButtonStyle(
+                            bgcolor = ft.Colors.BLUE_100
+                        )
+                    ),
+                    alignment = ft.alignment.center
+                )
+                ],
+                alignment = ft.MainAxisAlignment.SPACE_BETWEEN
             ),
             expand=True,
         )
